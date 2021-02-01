371d246c993538cf33fa0e540dc9b6247835b693
ibrahim ali asghar <mysterious.ibrahim@gmail.com>
2021-02-01 14:57:09 +0500

    added multiple folders

--- CleanArch/WebApi/Startup.cs
+++ CleanArch/WebApi/Startup.cs
@@ -1,7 +1,9 @@
+using CleanArch.Infra.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,11 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<DastavezDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DastavezDBContext"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
